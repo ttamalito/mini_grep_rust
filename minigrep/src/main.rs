cc4a5ed385d881e1c3c079800f6466881891aefd
Jose Gonzalez <coding.tamalito@gmail.com>
2024-03-22 15:21:07 +0100

    add logic to collect CLIarguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // collect all the command line arguments for the program
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args); // use the deug macro to print the arguments
+    let query = &args[1]; // the string that we have to look for in the file
+    let file_path = &args[2]; // the file to the path
+    println!("Looking for String: {}", query);
+    println!("In file: {}", file_path);
 }
