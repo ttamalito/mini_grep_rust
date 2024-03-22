9dac49613042de2990af4e1dd0e82b618ac3d59a
Jose Gonzalez <coding.tamalito@gmail.com>
2024-03-22 16:30:46 +0100

    refactor code

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,8 +4,9 @@ fn main() {
     // collect all the command line arguments for the program
     let args: Vec<String> = env::args().collect();
     //dbg!(args); // use the deug macro to print the arguments
-    let query = &args[1]; // the string that we have to look for in the file
-    let file_path = &args[2]; // the file to the path
+    let config = parse_config(&args);
+    let query = config.query; // the string that we have to look for in the file
+    let file_path = config.file_path; // the file to the path
     println!("Looking for String: {}", query);
     println!("In file: {}", file_path);
 
@@ -14,3 +15,31 @@ fn main() {
     // print the content
     println!("Te content of the file is: {}", content);
 }
+
+
+fn parse_config(args: &[String]) -> Config {
+    Config::new(args)
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+impl Config {
+    // create a constructor for the struct
+    fn new(args: &[String])-> Config {
+        // check if there are at least three elements in the args array
+        if args.len() < 3 {
+            panic!("There are not enoughparameters");
+        }
+        let query = &args[1];
+        let file_path = &args[2];
+    
+        Config {
+            query: query.clone(),
+            file_path: file_path.clone()
+        }
+    }
+}
+
