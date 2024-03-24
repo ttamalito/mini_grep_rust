6ab9395150c99089477e99ff5f378d8b482fcbd5
Jose Gonzalez <coding.tamalito@gmail.com>
2024-03-24 10:47:46 +0100

    refactor configuration

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,13 @@
-use std::{env, fs};
+use std::{env, fmt::Error, fs, process};
 
 fn main() {
     // collect all the command line arguments for the program
     let args: Vec<String> = env::args().collect();
     //dbg!(args); // use the deug macro to print the arguments
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem Parsing arguments: {err}");
+        process::exit(1);
+    });
     let query = config.query; // the string that we have to look for in the file
     let file_path = config.file_path; // the file to the path
     println!("Looking for String: {}", query);
@@ -40,6 +43,21 @@ impl Config {
             query: query.clone(),
             file_path: file_path.clone()
         }
+    }
+
+    // create another "constructor" for the Config struct
+    fn build(args: &[String])-> Result<Config, &str> {
+        // check if there are at least three elements in the args array
+        if args.len() < 3 {
+            return Err("Not enough CLI arguments");
+        }
+        let query = &args[1];
+        let file_path = &args[2];
+    
+        Result::Ok(        Config {
+            query: query.clone(),
+            file_path: file_path.clone()
+        })
     }
 }
 
